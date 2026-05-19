e17218de082473b129f43d8f3163b08386d52344
dhanifainance3-ai <dhanifainance3@gmail.com>
2026-05-19 03:28:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+// public folder ko static bana do
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Hello! Server chal raha hai 🚀');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(Server running on http://localhost:${PORT});
+  console.log(Server running on port ${PORT});
 });
